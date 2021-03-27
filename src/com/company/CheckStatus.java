eb97e2a5eca2ea292471e303289c62bbb323a729
nathan rhoden <nathanrhoden@nathans-MacBook-Pro.local>
2021-03-27 12:18:12 +0000

    added payer 2 winning combinations

--- src/com/company/CheckStatus.java
+++ src/com/company/CheckStatus.java
@@ -4,6 +4,7 @@ public class CheckStatus {
 
     public static void winingCombo(char[][] gameboard)
     {
+        // PLAYER 1 COMBINATIONS WINS
         if(gameboard[0][0]  == 'X' && gameboard[0][2] == 'X' && gameboard[0][4] =='X')
         {
             System.out.println("PLAYER 1 WINS");
@@ -42,6 +43,48 @@ public class CheckStatus {
         else if(gameboard[0][4]  == 'X' && gameboard[2][2] == 'X' && gameboard[4][0] =='X')
         {
             System.out.println("PLAYER 1 WINS");
+
+
+        }
+        //PLAYER 2 COMBINATION WINS
+        if(gameboard[0][0]  == 'O' && gameboard[0][2] == 'X' && gameboard[0][4] =='X')
+        {
+            System.out.println("PLAYER 2 WINS");
+
+        }
+        else if(gameboard[0][0]  == 'O' && gameboard[2][0] == 'X' && gameboard[4][0] =='X')
+        {
+            System.out.println("PLAYER 2 WINS");
+
+        }
+        else if(gameboard[0][0]  == 'O' && gameboard[2][2] == 'X' && gameboard[4][4] =='X')
+        {
+            System.out.println("PLAYER 2 WINS");
+
+        }
+        else if(gameboard[2][0]  == 'O' && gameboard[2][2] == 'X' && gameboard[2][4] =='X')
+        {
+            System.out.println("PLAYER 2 WINS");
+
+        }
+        else if(gameboard[4][0]  == 'O' && gameboard[4][2] == 'X' && gameboard[4][4] =='X')
+        {
+            System.out.println("PLAYER 2 WINS");
+
+        }
+        else if(gameboard[0][2]  == 'O' && gameboard[2][2] == 'X' && gameboard[4][2] =='X')
+        {
+            System.out.println("PLAYER 2 WINS");
+
+        }
+        else if(gameboard[0][4]  == 'O' && gameboard[2][4] == 'X' && gameboard[4][4] =='X')
+        {
+            System.out.println("PLAYER 2 WINS");
+
+        }
+        else if(gameboard[0][4]  == 'X' && gameboard[2][2] == 'X' && gameboard[4][0] =='X')
+        {
+            System.out.println("PLAYER 2 WINS");
 
 
         }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -8,6 +9,8 @@ public class Main {
 
 
         Scanner in = new Scanner(System.in);
+        Random rand = new Random();
+        int upperbound = 10;
 
         char[][] myObj = GameBoard.board(); // OBJ OF THE BOARD
         while(true) {
@@ -16,6 +19,7 @@ public class Main {
             GameBoard.printGameBoard(myObj);
 
             int userIn = in.nextInt(); // TAKES USER INPUT
+            int compIn = rand.nextInt(upperbound);
 
             switch (userIn) {
 
@@ -49,9 +53,44 @@ public class Main {
                     break;
 
             }
+            switch (compIn) {
+
+                case 1:
+                    myObj[0][0] = 'O';
+                    break;
+
+                case 2:
+                    myObj[0][2] = 'O';
+                    break;
+                case 3:
+                    myObj[0][4] = 'O';
+                    break;
+                case 4:
+                    myObj[2][0] = 'O';
+                    break;
+                case 5:
+                    myObj[2][2]= 'O';
+                    break;
+                case 6:
+                    myObj[2][4] = 'O';
+                    break;
+                case 7:
+                    myObj[4][0] = 'O';
+                    break;
+                case 8:
+                    myObj[4][2]= 'O';
+                    break;
+                case 9:
+                    myObj[4][4] = 'O';
+                    break;
+
+            }
 
             CheckStatus.winingCombo(myObj);
+
+
         }
+
 
 
     }
